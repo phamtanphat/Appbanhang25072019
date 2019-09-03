77f36ba61731fea4690ced60f23ff1307e539cdc
phatdroid94@gmail.com <phatdroid94@gmail.com>
2019-09-03 19:44:54 +0700

    init project

---
+++ App.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+
+import React, { Component } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends Component {
+  render() {
+    return (
+      <View>
+          <Text> App </Text>
+      </View>
+    );
+  }
+}
+
