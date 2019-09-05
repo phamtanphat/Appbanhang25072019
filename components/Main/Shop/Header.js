fbed7a0750443b126089024bd306dc1b9e7df13f
phatdroid94@gmail.com <phatdroid94@gmail.com>
2019-09-05 22:34:19 +0700

    A

--- components/Main/Shop/Header.js
+++ components/Main/Shop/Header.js
@@ -1,12 +1,39 @@
-import React, { PureComponent } from "react";
-import { Text, View } from "react-native";
+import React, { PureComponent } from 'react';
+import { Text, View, TouchableOpacity, Image, StyleSheet } from 'react-native';
+import icMenu from '../../../media/appIcon/ic_menu.png';
+import icLogo from '../../../media/appIcon/ic_logo.png';
+import Screenapp from '../../Unit/Screenapp';
 
 export default class Header extends PureComponent {
   render() {
+    const { container, row1, imageStyle, textStyle } = styles;
     return (
-      <View style={{ backgroundColor: "#34B089", flex: 1 }}>
-        <Text> Header </Text>
+      <View style={container}>
+        <View style={row1}>
+            <TouchableOpacity>
+                <Image source={icMenu} style={imageStyle} />
+            </TouchableOpacity>
+            <Text style={textStyle}>Wearing a Dress</Text>
+            <Image source={icLogo} style={imageStyle} />
+        </View>
+        <View />
       </View>
     );
   }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: '#34B089', 
+        flex: 1,
+        padding: Screenapp.width * 0.02
+    },
+    row1: {
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    imageStyle: {
+        width: Screenapp.width * 0.08,
+        height: Screenapp.height * 0.05
+    }
+});
---
+++ components/Unit/Screenapp.js
@@ -0,0 +1,4 @@
+import { Dimensions } from 'react-native';
+const Screenapp = Dimensions.get('window');
+export default Screenapp;
+
