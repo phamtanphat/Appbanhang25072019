291864aee33206d7c0d1893d9ad3d1abe723d314
phatdroid94@gmail.com <phatdroid94@gmail.com>
2019-09-05 21:00:16 +0700

    a

--- components/Main/Shop/Shop.js
+++ components/Main/Shop/Shop.js
@@ -1,14 +1,23 @@
 import React, { PureComponent } from 'react';
-import { View } from 'react-native';
+import { View, Image, StyleSheet, Dimensions } from 'react-native';
 import TabNavigator from 'react-native-tab-navigator';
 import Home from './Home/Home';
 import Cart from './Cart/Cart';
+import Search from './Search/Search';
+import Contact from './Contact/Contact';
 
-import ic_HomeSelect from '../../../media/appIcon/home0.png';
-import ic_HomeSelected from '../../../media/appIcon/home.png';
-import ic_CartSelect from '../../../media/appIcon/cart0.png';
-import ic_CartSelected from '../../../media/appIcon/cart.png';
 
+import icHomeSelect from '../../../media/appIcon/home0.png';
+import icHomeSelected from '../../../media/appIcon/home.png';
+import icCartSelect from '../../../media/appIcon/cart0.png';
+import icCartSelected from '../../../media/appIcon/cart.png';
+import icSearchSelect from '../../../media/appIcon/search0.png';
+import icSearchSelected from '../../../media/appIcon/search.png';
+import icContactSelect from '../../../media/appIcon/contact0.png';
+import icContactSelected from '../../../media/appIcon/contact0.png';
+
+
+const Screenheight = Dimensions.get('window').height;
 
 export default class Shop extends PureComponent {
   constructor(props) {
@@ -16,28 +25,68 @@ export default class Shop extends PureComponent {
     this.state = { selectedTab: 'Home' };
   }
   render() {
+    const { iconStyle } = styles;
     return (
-      <View style={{ flex : 1 }}>
-           <TabNavigator>
-              <TabNavigator.Item
-                  selected={this.state.selectedTab === 'Home'}
-                  title="Home"
-                  renderIcon
-                  onPress={() => this.setState({ selectedTab: 'Home' })}
-              >
-                  <Home />
-              </TabNavigator.Item>
-              <TabNavigator.Item
-                  selected={this.state.selectedTab === 'Cart'}
-                  title="Cart"
-                  onPress={() => this.setState({ selectedTab: 'Cart' })}
-              >
-                  <Cart />
-              </TabNavigator.Item>
-              
-          </TabNavigator>
+      <View style={{ flex: 1 }}>
+        <TabNavigator>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'Home'}
+            title="Home"
+            renderIcon={() => <Image source={icHomeSelect} style={iconStyle} />}
+            selectedTitleStyle={{ color: '#34B089', fontFamily: 'Avenir' }}
+            renderSelectedIcon={() => (
+              <Image source={icHomeSelected} style={iconStyle} />
+            )}
+            onPress={() => this.setState({ selectedTab: 'Home' })}
+          >
+            <Home />
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'Cart'}
+            title="Cart"
+            renderIcon={() => <Image source={icCartSelect} style={iconStyle} />}
+            renderSelectedIcon={() => (
+              <Image source={icCartSelected} style={iconStyle} />
+            )}
+            badgeText="1"
+            selectedTitleStyle={{ color: '#34B089', fontFamily: 'Avenir' }}
+            onPress={() => this.setState({ selectedTab: 'Cart' })}
+          >
+            <Cart />
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'Search'}
+            title="Search"
+            renderIcon={() => <Image source={icSearchSelect} style={iconStyle} />}
+            selectedTitleStyle={{ color: '#34B089', fontFamily: 'Avenir' }}
+            renderSelectedIcon={() => (
+              <Image source={icSearchSelected} style={iconStyle} />
+            )}
+            onPress={() => this.setState({ selectedTab: 'Search' })}
+          >
+            <Home />
+          </TabNavigator.Item>
+          <TabNavigator.Item
+            selected={this.state.selectedTab === 'Contact'}
+            title="Contact"
+            renderIcon={() => <Image source={icContactSelect} style={iconStyle} />}
+            selectedTitleStyle={{ color: '#34B089', fontFamily: 'Avenir' }}
+            renderSelectedIcon={() => (
+              <Image source={icContactSelected} style={iconStyle} />
+            )}
+            onPress={() => this.setState({ selectedTab: 'Contact' })}
+          >
+            <Home />
+          </TabNavigator.Item>
+        </TabNavigator>
       </View>
     );
   }
 }
-// https://github.com/ptomasroos/react-native-tab-navigator
+
+const styles = StyleSheet.create({
+  iconStyle: {
+    height: Screenheight * 0.04,
+    width: Screenheight * 0.04
+  }
+});
