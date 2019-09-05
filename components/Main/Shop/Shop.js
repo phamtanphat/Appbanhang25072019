80a8e8e7829f1eee886e7c86714389cdbe878140
phatdroid94@gmail.com <phatdroid94@gmail.com>
2019-09-05 20:40:03 +0700

    a

--- components/Main/Shop/Shop.js
+++ components/Main/Shop/Shop.js
@@ -4,6 +4,10 @@ import TabNavigator from 'react-native-tab-navigator';
 import Home from './Home/Home';
 import Cart from './Cart/Cart';
 
+import ic_HomeSelect from '../../../media/appIcon/home0.png';
+import ic_HomeSelected from '../../../media/appIcon/home.png';
+import ic_CartSelect from '../../../media/appIcon/cart0.png';
+import ic_CartSelected from '../../../media/appIcon/cart.png';
 
 
 export default class Shop extends PureComponent {
